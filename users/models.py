53b67348424b880a622553b5da519f260283b680
Pablo Seoane <pablitoseoane@gmail.com>
2022-06-28 12:13:34 -0300

    models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="avatares", null=True, blank=True)
