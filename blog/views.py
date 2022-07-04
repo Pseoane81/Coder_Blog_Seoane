5acec9d7112e6798cf96258914af0e7191052483
Pablo Seoane <pablitoseoane@gmail.com>
2022-07-04 15:35:28 -0300

    arreglo home

--- blog/views.py
+++ blog/views.py
@@ -9,15 +9,15 @@ from users.models import User, avatar
 
 def home(request):
     post= Post.objects.all()
-    user = request.user
     avatares = avatar.objects.filter(user=request.user.id)
     if len(avatares) > 0:
-        return render(request, 'home.html',{"posts":post, "user":user.username,"img":avatares[0].image.url})
+        return render(request, 'home.html',{"posts":post,"img":avatares[0].image.url})
     else:
         return render(request, 'home.html',{"posts":post})
 
 
 def about(request):
+
     return render(request, 'about.html')
     
     
